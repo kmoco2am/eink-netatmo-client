4f43a58c5bbd260600c14208eeba4d33d18cfce7
okmoch_pure <okmoch@purestorage.com>
2023-11-22 22:53:25 +0100

    Ignore output images

--- upload-code.sh
+++ upload-code.sh
@@ -9,4 +9,6 @@ rsync -avz \
   --exclude '.idea' \
   --exclude 'image.bmp' \
   --exclude 'output' \
+  --exclude '__pycache__' \
+  --exclude 'bitmap_frame_*.py' \
   . pi:/home/pi/workspace/eink-py
